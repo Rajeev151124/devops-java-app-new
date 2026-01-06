3efd75408bd3949d72a35cfe3d932ced1ced06bd
admin <Rajeev151124>
2026-01-06 14:19:55 +0530

    2nd commit

--- src/main/java/com/devops/app/App.java
+++ src/main/java/com/devops/app/App.java
@@ -1,7 +1,20 @@
 package com.devops.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class App {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello DevOps! Application is running successfully.";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello DevOps! Application is running successfully.");
+        SpringApplication.run(App.class, args);
     }
 }
